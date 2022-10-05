31ab6333aad331310b8f2af008ef174a91d3bd3f
+ <+>
2022-10-05 13:10:29 +0300

    completed code challenge

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -9,13 +9,13 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotCard({ bot }) {
+function BotCard({ bot, removeBotFromArmy, onSelectBot }) {
   return (
-    <div className="ui column">
+     <div className="ui column">
       <div
         className="ui card"
         key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        onClick={() => onSelectBot(bot)}
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
@@ -48,8 +48,7 @@ function BotCard({ bot }) {
               <button
                 className="ui mini red button"
                 onClick={() =>
-                  console.log("add code to connect event listener")
-                }
+                  removeBotFromArmy(bot.id)}
               >
                 x
               </button>
