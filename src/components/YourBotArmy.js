e952e51086fef144817a6d0d026b63f35bfda521
+ <+>
2022-10-05 13:08:58 +0300

    completed code challenge

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,18 +1,21 @@
 import React from "react";
+import BotCard from "./BotCard";
 
-function YourBotArmy() {
-  //your bot army code here...
 
+function YourBotArmy(bots, selectBot, removeBotFromArmy) {
   return (
     <div className="ui segment inverted olive bot-army">
       <div className="ui five column grid">
         <div className="row bot-army-row">
-          {/*...and here...*/}
+          {bots.map((bot)=>(
+            <BotCard key={bot.id} bot={bot} onSelectBot={selectBot} removeBotFromArmy={removeBotFromArmy} />
+          ))}
           Your Bot Army
         </div>
       </div>
     </div>
   );
 }
+
 
 export default YourBotArmy;
